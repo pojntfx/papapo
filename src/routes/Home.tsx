59525ec4ba5441ac026f78394cc708eeb96dd761
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-18 21:09:59 +0200

    feat: add semantic-ui

---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { DefaultLayout } from "../layouts/Default";
+
+const Home = props => (
+  <DefaultLayout {...props}>
+    <h1>PaPaPo</h1>
+  </DefaultLayout>
+);
+
+export { Home };
