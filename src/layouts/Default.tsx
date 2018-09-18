81ebb71eb326f9da5c4fea57034dde470e518c21
Felix Pojtinger <felix@pojtinger.com>
2018-09-18 22:38:09 +0200

    feat: add background

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -3,6 +3,16 @@ import { Container } from "semantic-ui-react";
 import { Navbar } from "../components/navigation/Navbar";
 import { AppBar } from "../components/navigation/AppBar";
 import styled from "styled-components";
+import { injectGlobal } from "styled-components";
+/// <reference path='../../types/logo.d.ts'/>
+import background from "../assets/bg.jpg";
+
+injectGlobal`
+  body {
+    background: url(${background}) no-repeat center center fixed;
+    background-size: cover;
+  }
+`;
 
 const DefaultContainer = styled(Container)`
   padding-top: 1em;
