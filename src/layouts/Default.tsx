8124967e1222a459db5cce97bd79cfc3790b5245
Felix Pojtinger <felix@pojtinger.com>
2018-09-18 22:27:46 +0200

    feat: add app bar and logo

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import { Container } from "semantic-ui-react";
 import { Navbar } from "../components/navigation/Navbar";
+import { AppBar } from "../components/navigation/AppBar";
+import styled from "styled-components";
+
+const DefaultContainer = styled(Container)`
+  padding-top: 1em;
+`;
 
 const DefaultLayout = ({ children, ...props }) => (
   <React.Fragment>
+    <AppBar />
     <Navbar />
-    <Container {...props}>{children}</Container>
+    <DefaultContainer {...props}>{children}</DefaultContainer>
   </React.Fragment>
 );
 
