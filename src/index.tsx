494831fb249ea5f9aa62ae534d033e3928cea737
Felix Pojtinger <felix@pojtinger.com>
2018-09-18 20:47:12 +0200

    feat: use Parcel instead of CRA

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
 
-ReactDOM.render(<h1>PaPaPo</h1>, document.getElementById("root"));
+render(<h1>PaPaPo</h1>, document.getElementById("root"));
