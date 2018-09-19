1bc5be7ade23c68988deb97d975f108dcb2d223d
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-19 19:06:58 +0200

    feat: add more routes, refactor layout

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import { Fragment } from "react";
 import { Container } from "semantic-ui-react";
-import { Navbar } from "../components/navigation/Navbar";
 import { AppBar } from "../components/navigation/AppBar";
-import styled from "styled-components";
 import { injectGlobal } from "styled-components";
 /// <reference path='../../types/logo.d.ts'/>
 import background from "../assets/bg.jpg";
-import { TagBar } from "../components/navigation/TagBar";
 import { Footer } from "../components/navigation/Footer";
 
 injectGlobal`
@@ -20,8 +17,6 @@ injectGlobal`
 const DefaultLayout = ({ children, ...props }) => (
   <Fragment>
     <AppBar />
-    <Navbar />
-    <TagBar />
     <Container {...props}>{children}</Container>
     <Footer />
   </Fragment>
---
+++ src/pages/UploadVideo.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { DefaultLayout } from "../layouts/Default";
+import { Segment, Button } from "semantic-ui-react";
+import styled from "styled-components";
+
+const Wrapper = styled(Segment)`
+  margin-top: 1em !important;
+`;
+
+const UploadVideo = props => (
+  <DefaultLayout {...props}>
+    <Wrapper>
+      <h1>Upload Video</h1>
+      <Button content="Upload" />
+    </Wrapper>
+  </DefaultLayout>
+);
+
+export { UploadVideo };
