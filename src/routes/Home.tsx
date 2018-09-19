f19046acd50a931c37c2b5cbd60eef87076da9b3
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-19 18:36:36 +0200

    feat: add React Router

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,1 @@
-import * as React from "react";
-import { DefaultLayout } from "../layouts/Default";
-import { VideoCards } from "../components/feed/VideoCards";
-
-const Home = props => (
-  <DefaultLayout {...props}>
-    <VideoCards />
-  </DefaultLayout>
-);
-
-export { Home };
+export { Home } from "../pages/Home";
