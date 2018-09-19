04443ac549283f960dd6c6e97b8df09e2c80eb85
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-19 17:54:02 +0200

    refactor(Tags, Videos): get data from JSON files

--- src/components/feed/VideoCards.tsx
+++ src/components/feed/VideoCards.tsx
@@ -2,31 +2,8 @@ import * as React from "react";
 import { VideoCard } from "./VideoCard";
 import { Card } from "semantic-ui-react";
 import styled from "styled-components";
-
-const videos = [
-  {
-    url: "https://www.youtube.com/watch?v=006ip4ndThE",
-    title: "Giant panda bear does handstand!",
-    publishDate: "3 hours",
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-    pariatur aspernatur optio ipsum dolorum quisquam accusantium incidunt
-    iusto ratione harum maxime laudantium atque.`,
-    viewCount: 3206,
-    likeCount: 37,
-    shareCount: 12
-  },
-  {
-    url: "https://www.youtube.com/watch?v=006ip4ndThE",
-    title: "Giant panda bear does handstand!",
-    publishDate: "3 hours",
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-    pariatur aspernatur optio ipsum dolorum quisquam accusantium incidunt
-    iusto ratione harum maxime laudantium atque.`,
-    viewCount: 3206,
-    likeCount: 37,
-    shareCount: 12
-  }
-];
+/// <reference path="../../types/json.d.ts"/>
+import videos from "../../data/videos.json";
 
 const CardGroup = styled(Card.Group)`
   @media (max-width: 767px) {
